732cf59f1fd32e79dc55ca2c388f047f0754de74
ThatcherK <kaliithatmau@gmail.com>
2023-02-07 16:17:30 +0300

    Delete unused charts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import './state';
-// import renderD3Chart from './charts/d3/exampleChart';
-// import renderEChart from './core/ExampleChart';
-// import renderTable from './core/ExampleTable';
 import renderEconomicPovertyMap from './core/EconomicPovertyMap';
 import renderEconomicPovertyBarchart from './core/EconomicPovertyBarChart';
 import './styles/styles.css';
@@ -11,8 +8,5 @@ import './styles/styles.css';
  */
 window.addEventListener('load', () => {
   renderEconomicPovertyMap();
-  // renderD3Chart();
-  // renderEChart();
-  // renderTable('dicharts--table-example');
   renderEconomicPovertyBarchart();
 });
